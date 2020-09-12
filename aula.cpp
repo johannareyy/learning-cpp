99f079913db7c761e368e3a8d99a0dde05454139
Thales Minoru Sato <thalesms2@edu.univali.br>
2020-09-12 15:04:27 -0300

    lala

---
+++ aula.cpp
@@ -0,0 +1,17 @@
+[[nodiscard]]
+
+int t{1};
+
+
+sdt::vector<std::pair<double,double>> lados{3.0,4.0}, {5.0, 12.0}, {8.0, 15.0}};
+
+for(auto triangulo:lados){
+    std::cout << "Triangulo " <<(t++)>> "hipotenusa:"
+              << hipotenusa(triangulo.first, triangulo.second) std::endl;
+
+
+}
+
+vector < pair<double, double> > lados
+
+pares de variaveis
